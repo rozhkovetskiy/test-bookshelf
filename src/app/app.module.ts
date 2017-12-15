bf9b1ca8b7a427650b16efe18141a00ba7711dba
rozhkovetskiy <rozhkovetskiy@gmail.com>
2017-12-15 15:13:39 +0300

    init repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './main/app.component';
+import { ListBooksComponent } from './list-books/list-books.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListBooksComponent,
+    BookDetailsComponent
   ],
   imports: [
     BrowserModule
